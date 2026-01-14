8c0a4c7ecfaa79a9320e72a89ec730142fb348b5
andhikadk <andhikadwi980@gmail.com>
2026-01-14 17:09:43 +0700

    chore: remove apple-touch-icon reference and file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,7 +33,6 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600|jetbrains-mono:400,500,600,700"
